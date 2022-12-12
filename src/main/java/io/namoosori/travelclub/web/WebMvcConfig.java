0ad9e4c48230fd6663e563c9dfdfbfcda6a8db4f
Jyonyve <keyneco@gmail.com>
2022-12-13 08:29:46 +0900

    delete 기능 구현

---
+++ src/main/java/io/namoosori/travelclub/web/WebMvcConfig.java
@@ -0,0 +1,28 @@
+package io.namoosori.travelclub.web;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@RequiredArgsConstructor
+@Configuration
+public class WebMvcConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+            registry.addMapping("/**")
+            .allowedOrigins("http://localhost:3000")
+            .allowedOrigins("http://localhost:3001")
+            .allowedOrigins("http://localhost:3002")
+            .allowedMethods(
+                HttpMethod.GET.name(),
+                HttpMethod.HEAD.name(),
+                HttpMethod.POST.name(),
+                HttpMethod.PUT.name(),
+                HttpMethod.DELETE.name(),
+                HttpMethod.OPTIONS.name()
+            );
+            }
+}
